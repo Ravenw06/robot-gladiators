9b4294903383e19e578715d79e3d94045035bc45
Your Name <you@example.com>
2021-03-12 20:08:46 -0500

    defeat ALL enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,12 +12,13 @@ var playerMoney = 10;
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]; 
 var enemyHealth = 50;
 var enemyAttack = 12;
-
+console.log(' I am in');
 
 
 var fight = function(enemyName){
-  
-  //Alert players that they are starting the round
+  //repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
+    //Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
   var promptFight = window.prompt("Would you like to FIGHT or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -50,7 +51,8 @@ var fight = function(enemyName){
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
     // if player choses to skip
-  } else if (promptFight === "skip" || promptFight === "SKIP") {
+  }
+     else if (promptFight === "skip" || promptFight === "SKIP") {
     //confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
     //if yes(ture), laeve fight
@@ -63,12 +65,14 @@ var fight = function(enemyName){
     }
   } else {
     window.alert("You need to choose a valid option. Try again!");
+    }
+
   }
-  
-  
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
 
